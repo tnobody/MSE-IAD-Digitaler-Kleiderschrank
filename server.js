1d97c8f797018a79c4a3d95626ab8016a5fdc9b5
tim <tim.keiner@consol.de>
2015-11-26 23:30:46 +0100

    added port throug env

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('app'));
-var server = app.listen(80,function() {
+var server = app.listen(app.get('port'),function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
