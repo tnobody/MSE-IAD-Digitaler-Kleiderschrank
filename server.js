2a9f4ed3b4cd9a5019d420ebe6630c786119e62b
tim <tim.keiner@consol.de>
2015-11-26 23:24:19 +0100

    added static file server in start script

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.use(express.static('app'));
+var server = app.listen(80,function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+})
