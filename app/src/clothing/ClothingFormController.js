0db33a5c1a938dd611aad6b1d9337157b24553a1
tim <tim.keiner@consol.de>
2016-01-17 22:12:08 +0100

    - Clothing Editor for presentation

--- app/src/clothing/ClothingFormController.js
+++ app/src/clothing/ClothingFormController.js
@@ -7,14 +7,40 @@ ClothingFormController.$inject = ['$mdDialog', '$log']
 function ClothingFormController($mdDialog, $log) {
 
     this.categories = [
-        {id: 1, name: 'Shirt'},
+        {id: 1, name: 'T-Shirt'},
         {id: 2, name: 'Pullover'},
         {id: 3, name: 'Jacke'},
         {id: 4, name: 'Hemd'},
         {id: 5, name: 'Polo'},
     ];
+
+    this.colors = [
+        {name: 'Schwarz',code: '#000000'},
+        {name: 'Grau', code: '#888888'},
+        {name: 'Weiß', code: '#FFFFFF'},
+        {name: 'Rot', code: '#FF0000'},
+        {name: 'Dunkelrot', code: '#880000'},
+        {name: 'Grün', code: '#00FF00'},
+        {name: 'Dunkelgrün', code: '#008800'},
+        {name: 'Blau', code: '#0000FF'},
+        {name: 'Dunkelblau', code: '#000088'},
+        {name: 'Cyan', code: '#00FFFF'},
+        {name: 'Dunkelcyan', code: '#008888'},
+        {name: 'Magenta', code: '#FF00FF'},
+        {name: 'Dunkelmagenta', code: '#880088'},
+        {name: 'Gelb', code: '#FFFF00'},
+        {name: 'Dunkelgelb', code: '#888800'},
+        {name: 'Blaugrün', code: '#00FF88'},
+        {name: 'Himmelblau', code: '#0088FF'},
+        {name: 'Rosé', code: '#FF0088'},
+        {name: 'Violett', code: '#8800FF'},
+        {name: 'Orange', code: '#FF8800'},
+        {name: 'Gelbgrün', code: '#88FF00'},
+    ].sort((c) => c.name);
+
     this.category = this.categories[2];
 
+    /* Merkmale */
     this.possibleCharacteristic = [
         {name: 'V-Ausschnitt'},
         {name: 'Sport'},
@@ -23,14 +49,28 @@ function ClothingFormController($mdDialog, $log) {
     ]
     this.characteristics = [];
     this.characteristic = {};
-    this.characteristicSearch = 'ALalal';
+    this.characteristicSearch = '';
     this.findCharacteristic = (search) => {
-        $log.debug(search);
         return search ? this.possibleCharacteristic.filter((c) => {
-            return c.name.indexOf(search) >= 0
+            return c.name.toLowerCase().indexOf(search.toLowerCase()) >= 0
         }) : [];
     }
+
     this.mapCharacteristic = (c) => {
+        return c;
+    };
+
+    /* Weitere farben */
+    this.additionalColors = [];
+    this.additionalColor = {};
+    this.additionalColorSearch = '';
+    this.findColors = (search) => {
+        return search ? this.colors.filter((c) => {
+            return c.name.toLowerCase().indexOf(search.toLowerCase()) >= 0
+        }) : [];
+    }
+
+    this.mapColor = (c) => {
         return c;
     };
 
@@ -40,5 +80,6 @@ function ClothingFormController($mdDialog, $log) {
     this.cancel = () => {
         $mdDialog.cancel();
     };
+
 
 }
